2019e9290358d9ec9a22d7b07a0bbe22dd128a5b
LLSoft <louislanctot@gmail.com>
2017-06-29 20:47:47 -0400

    Initial Commit

---
+++ Ptv/Ptv.h
@@ -0,0 +1,19 @@
+//
+//  Ptv.h
+//  Ptv
+//
+//  Created by Louis on 2017-06-29.
+//  Copyright © 2017 LLSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Ptv.
+FOUNDATION_EXPORT double PtvVersionNumber;
+
+//! Project version string for Ptv.
+FOUNDATION_EXPORT const unsigned char PtvVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ptv/PublicHeader.h>
+
+
